3b184cc485dfc98fbd1d6d125b314b7e8ea25012
Alex Harston <alex@harston.io>
2020-09-27 01:15:19 +0100

    changed to meals and added error handling if no meal found

--- mfp-lytiko.py
+++ mfp-lytiko.py
@@ -2,57 +2,115 @@ import requests
 import myfitnesspal
 import kirjava
 import pytz
-from datetime import datetime, date, time, timezone
-from secrets import LYTIKO_USERNAME, LYTIKO_PASSWORD, USERNAME, PASSWORD, CALORIES_ID, SUGAR_ID, WEIGHT_ID, CARBOHYDRATES_ID, FAT_ID, PROTEIN_ID, SODIUM_ID
+from datetime import datetime, date, time, timezone, timedelta
+from secrets import LYTIKO_USERNAME, LYTIKO_PASSWORD, USERNAME, PASSWORD, WEIGHT_ID
 
-
-
-DATE = int(datetime.now().timestamp())
 today = date.today()
+yesterday = datetime.today() -timedelta(days=1)
+yesterday_breakfast = int(datetime(yesterday.year, yesterday.month, yesterday.day, 9, 0, 0 ).timestamp())
+yesterday_lunch = int(datetime(yesterday.year, yesterday.month, yesterday.day, 13, 0, 0 ).timestamp())
+yesterday_snacks = int(datetime(yesterday.year, yesterday.month, yesterday.day, 16, 0, 0 ).timestamp())
+yesterday_dinner = int(datetime(yesterday.year, yesterday.month, yesterday.day, 19, 0, 0 ).timestamp())
+
 dt = int(datetime.now().timestamp())
-print('dt: ', dt)
 #connect to MyFitnessPal with credentials
 client = myfitnesspal.Client(username=USERNAME, password=PASSWORD)
 
 #get today's information
-day = client.get_date(today.year, today.month, today.day)
+day = client.get_date(yesterday.year, yesterday.month, yesterday.day)
+print('DAY', day)
 
 #collect data from MFP API
 breakfast = day.meals[0]
 lunch = day.meals[1]
 dinner = day.meals[2]
 snacks = day.meals[3]
+print('B', breakfast)
+print('L', lunch)
+print('D', dinner)
+print('S', snacks)
 
-totals = day.totals
-weight = list(client.get_measurements('Weight').items())[-1][1]
-print(totals)
-nutrients_mutation = """
+#weight = list(client.get_measurements('Weight').items())[-1][1]
+
+breakfast_mutation = """
     mutation {createMeal(
-        rawCalories: """ + str(int(totals['calories'])) +""",
-        rawCarbohydrates: """ + str(int(totals['carbohydrates'])) +""",
-        rawFat: """ + str(int(totals['fat'])) +""",
-        rawProtein: """ + str(int(totals['protein'])) +""",
-        rawSugar: """ + str(int(totals['sugar'])) +""",
-        rawSalt: """ + str(int(totals['sodium']/1000)) +""",
+        rawCalories: """ + str(int(breakfast.totals['calories'] if breakfast else 0)) +""",
+        rawCarbohydrates: """ + str(int(breakfast.totals['carbohydrates'] if breakfast else 0)) +""",
+        rawFat: """ + str(int(breakfast.totals['fat'] if breakfast else 0)) +""",
+        rawProtein: """ + str(int(breakfast.totals['protein'] if breakfast else 0)) +""",
+        rawSugar: """ + str(int(breakfast.totals['sugar'] if breakfast else 0)) +""",
+        rawSalt: """ + str(int(breakfast.totals['sodium'] if breakfast else 0)) +""",
         components: [],
         timezone: "Europe/London",
-        datetime:"""+ str(dt) + """
+        datetime:""" + str(yesterday_breakfast) + """
     )
     { meal {id}}
     }
 """
-print(nutrients_mutation)
+print(breakfast_mutation)
 
-weight_mutation = """
-    mutation {createMeasurement(
-        quantity: """ + WEIGHT_ID + """,
-        value:""" + str(weight) + """,
+lunch_mutation = """
+    mutation {createMeal(
+        rawCalories: """ + str(int(lunch.totals['calories'] if lunch else 0)) +""",
+        rawCarbohydrates: """ + str(int(lunch.totals['carbohydrates'] if lunch else 0)) +""",
+        rawFat: """ + str(int(lunch.totals['fat'] if lunch else 0)) +""",
+        rawProtein: """ + str(int(lunch.totals['protein'] if lunch else 0)) +""",
+        rawSugar: """ + str(int(lunch.totals['sugar'] if lunch else 0)) +""",
+        rawSalt: """ + str(int(lunch.totals['sodium'] if lunch else 0)) +""",
+        components: [],
         timezone: "Europe/London",
-        datetime:"""+ str(DATE) + """
+        datetime:""" + str(yesterday_lunch) + """
     )
-    { measurement {id}}
+    { meal {id}}
     }
 """
+print(lunch_mutation)
+
+dinner_mutation = """
+    mutation {createMeal(
+        rawCalories: """ + str(int(dinner.totals['calories'] if dinner else 0)) +""",
+        rawCarbohydrates: """ + str(int(dinner.totals['carbohydrates'] if dinner else 0)) +""",
+        rawFat: """ + str(int(dinner.totals['fat'] if dinner else 0)) +""",
+        rawProtein: """ + str(int(dinner.totals['protein'] if dinner else 0)) +""",
+        rawSugar: """ + str(int(dinner.totals['sugar'] if dinner else 0)) +""",
+        rawSalt: """ + str(int(dinner.totals['sodium'] if dinner else 0)) +""",
+        components: [],
+        timezone: "Europe/London",
+        datetime:""" + str(yesterday_dinner) + """
+    )
+    { meal {id}}
+    }
+"""
+print(dinner_mutation)
+
+snacks_mutation = """
+    mutation {createMeal(
+        rawCalories: """ + str(int(snacks.totals['calories'] if snacks else 0)) +""",
+        rawCarbohydrates: """ + str(int(snacks.totals['carbohydrates'] if snacks else 0)) +""",
+        rawFat: """ + str(int(snacks.totals['fat'] if snacks else 0)) +""",
+        rawProtein: """ + str(int(snacks.totals['protein'] if snacks else 0)) +""",
+        rawSugar: """ + str(int(snacks.totals['sugar'] if snacks else 0)) +""",
+        rawSalt: """ + str(int(snacks.totals['sodium'] if snacks else 0)) +""",
+        components: [],
+        timezone: "Europe/London",
+        datetime:""" + str(yesterday_snacks) + """
+    )
+    { meal {id}}
+    }
+"""
+print(snacks_mutation)
+
+
+#weight_mutation = """
+#    mutation {createMeasurement(
+#        quantity: """ + WEIGHT_ID + """,
+#        value:""" + str(weight) + """,
+#        timezone: "Europe/London",
+#        datetime:"""+ str(dt) + """
+#    )
+#    { measurement {id}}
+#    }
+#"""
 
 login_mutation = """
     mutation {login(
@@ -65,4 +123,7 @@ login_mutation = """
 lytiko_url = "https://api.lytiko.com/graphql"
 token = (kirjava.execute(lytiko_url, login_mutation))
 token = token['data']['login']['token']
-print(kirjava.execute(lytiko_url, nutrients_mutation, headers={"Authorization": f"{token}"}))
+print(kirjava.execute(lytiko_url, breakfast_mutation, headers={"Authorization": f"{token}"}))
+print(kirjava.execute(lytiko_url, lunch_mutation, headers={"Authorization": f"{token}"}))
+print(kirjava.execute(lytiko_url, dinner_mutation, headers={"Authorization": f"{token}"}))
+print(kirjava.execute(lytiko_url, snacks_mutation, headers={"Authorization": f"{token}"}))
